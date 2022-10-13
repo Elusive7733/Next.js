f96e63be2b6b90be26f8504f53639b2a56aa5c57
Elusive7733 <abdullaharaheel@gmail.com>
2022-10-13 20:43:15 +0500

    Navigating through Links
    Dynamic List of links

--- next-tutorial/pages/index.js
+++ next-tutorial/pages/index.js
@@ -1,4 +1,5 @@
 import styles from "../styles/Home.module.css";
+import Link from 'next/link';
 
 // The function below behaves like a react component
 // The component below is Pre-Rendered to an HTML
@@ -7,7 +8,19 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-    <h1>Hello Next world!</h1>
+      <h1>Hello Next world!</h1>
+      <ul>
+        <li>
+          {/* Using an Anchor tag have a disadvantage, We send a new http request to load this new page,
+          any application state will be lost, even the state on redux / context */}
+          {/* <a href="/portfolio"></a> */}
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          {/* Replace prop replaces the page so we can't access history {Can't go back} */}
+          <Link replace href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 }
