5352225cb69f68647861dd80e475acaa1c4d0802
Elusive7733 <abdullaharaheel@gmail.com>
2022-10-26 21:27:23 +0500

    Context in getServerSideProps

--- next-pre-rendering/pages/profile.js
+++ next-pre-rendering/pages/profile.js
@@ -9,13 +9,19 @@ const Profile = (props) => {
 export default Profile;
 
 
+//* If we want display a page with highly dynamic data that changes with every request we can use server side rendering
+//. If we want to handle every incoming request by using server-side rendering we can use getServerSideProps()
+// ---> This is useful if we want to fetch data from an external API
+// ---> This does not pre generate pages but instead renders the page on every request
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async (context) => {
+  const { params, req, res } = context;
 
-
-    return {
-        props:{
-            name: "Abdullah"
-        }
-    }
+  //we get node req and res objects in getServerSideProps context, but also the params object similar to getStaticProps
+    
+  return {
+    props: {
+      name: "Abdullah",
+    },
+  };
 }
