a4dc27ebee3ad5b29c15a58b28239924c7f53e2d
Abdullah Raheel <Abdullaharaheel@gmail.com>
2023-01-04 15:30:48 +0500

    Update profile.js

--- next-pre-rendering/pages/profile.js
+++ next-pre-rendering/pages/profile.js
@@ -9,6 +9,7 @@ const Profile = (props) => {
 export default Profile;
 
 
+
 //* If we want display a page with highly dynamic data that changes with every request we can use server side rendering
 //. If we want to handle every incoming request by using server-side rendering we can use getServerSideProps()
 // ---> This is useful if we want to fetch data from an external API
